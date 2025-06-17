b675d3add1caedbf5f6efa3dd56ef5b79a38c653
Eugenpak <techteamit@yandex.ru>
2025-06-17 18:02:09 +0300

    Добавить пакет src\main\java для подмодуля ewm-stats-service\dto.

---
+++ ewm-stats-service/dto/src/main/java/ru/practicum/EndpointHit.java
@@ -0,0 +1,32 @@
+package ru.practicum;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class EndpointHit {
+    @NotBlank
+    @Size(max = 100)
+    private String app;
+
+    @NotBlank
+    @Size(max = 255)
+    private String uri;
+
+    @NotBlank
+    @Size(max = 50)
+    private String ip;
+
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime timestamp;
+}
